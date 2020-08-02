103999280ec6ba845d6a96fe1dbe4b6689f0d7c3
Murilo Reis <muriloreisriosalves@gmail.com>
2020-08-02 18:15:34 -0300

    Fixing formGroup class on contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,15 +6,15 @@ export default function Contact(props) {
     return (
         <div className={classes.Contact}>
             <div className='wrapper'>
-                <div className={classes.formGroup}>
+                <div className="formGroup">
                     <label>Name</label>
                     <input type="text" name="name" id="name"/>
                 </div>
-                <div className={classes.formGroup}>
+                <div className="formGroup">
                     <label>Email</label>
                     <input type="email" name="email" id="email"/>
                 </div>
-                <div className={classes.formGroup}>
+                <div className="formGroup">
                     <textarea name="content" id="content" cols="30" rows="10"></textarea>
                 </div>
             </div>
