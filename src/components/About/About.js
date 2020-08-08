15251f5bc85848147246d05a9554f02217e4edae
Murilo Reis <muriloreisriosalves@gmail.com>
2020-08-07 21:55:23 -0300

    Adding avatar img, and styling to the about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Axios from 'axios';
 import classes from './About.module.css';
+import avatar from '../../assets/images/avatar.jpeg'
 
 export default function About(props) {
     const [owner, setOwner] = useState({})
@@ -21,7 +22,10 @@ export default function About(props) {
     return (
         <div className={classes.About}>
             <div className="wrapper">
-                <h2>{owner.name}</h2>    
+                <div className="card about">
+                    <img src={ avatar } alt=""/>
+                    <p>Olá meu nome é Murilo Alves sou Analista de Sistemas Jr na Capgemini, desenvolvedor NodeJs e ReactJs, nesse portal você ira encontrar um pouco do meu trabalho, caso goste pode entrar em contato. </p>
+                </div>
             </div>
         </div>
     )
