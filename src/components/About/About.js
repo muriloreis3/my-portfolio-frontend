b8f48ffd445c9242e73fc38243225e47d5ddc6b8
Murilo Reis <muriloreisriosalves@gmail.com>
2020-08-10 19:23:33 -0300

    reseting styles and adjusting about page layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,8 @@ import classes from './About.module.css';
 import avatar from '../../assets/images/avatar.jpeg'
 
 export default function About(props) {
-    const [owner, setOwner] = useState({})
+    const [owner, setOwner] = useState({});
+
     useEffect(() => {
         Axios.get('http://localhost:5000/owner',{
             headers: {                  
@@ -24,7 +25,7 @@ export default function About(props) {
             <div className="wrapper">
                 <div className="card about">
                     <img src={ avatar } alt=""/>
-                    <p>Olá meu nome é Murilo Alves sou Analista de Sistemas Jr na Capgemini, desenvolvedor NodeJs e ReactJs, nesse portal você ira encontrar um pouco do meu trabalho, caso goste pode entrar em contato. </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum repudiandae aperiam laborum ducimus mollitia sit, quasi deserunt necessitatibus esse eaque?</p>
                 </div>
             </div>
         </div>
