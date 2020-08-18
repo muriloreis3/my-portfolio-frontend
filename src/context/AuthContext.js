6b250d6d1fc09e7f7d7936045703c68ebc373144
Murilo Reis <muriloreisriosalves@gmail.com>
2020-08-17 21:43:11 -0300

    Refactoring of AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
+import * as API from "../api/api";
 
-const AuthContext = React.createContext();
+export const AuthContext = React.createContext({
+  token: null,
+  error: null,
+  login: () => {},
+  logout: () => {},
+});
 
-export default AuthContext;
+const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+  const [error, setError] = useState(null);
+
+  const login = (password) => {
+    API.login(password)
+      .then((resp) => {
+        setToken(resp);
+        setError(null);
+        localStorage.setItem("token", resp);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    setError(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ token: token, error: error, login: login, logout: logout }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
