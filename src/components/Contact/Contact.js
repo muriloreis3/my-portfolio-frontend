463624881ce83cdc057fb2aaefe57b4d5b9e5e12
Murilo Reis <muriloreisriosalves@gmail.com>
2020-08-19 19:10:43 -0300

    Creation of form component, and refactoring of contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 import classes from './Contact.module.css'
+import Form from '../UI/Form/form';
 
 export default function Contact(props) {
+    const send = (message) => {
+        console.log(message);
+    }
+    
     return (
         <div className={classes.Contact}>
-            <div className='wrapper'>
+            <Form submited={send}>
                 <div className="formGroup">
                     <label>Name</label>
                     <input type="text" name="name" id="name"/>
@@ -18,7 +23,7 @@ export default function Contact(props) {
                     <label>Message</label>
                     <textarea name="content" id="content"></textarea>
                 </div>
-            </div>
+            </Form>        
         </div>
     )
 }
