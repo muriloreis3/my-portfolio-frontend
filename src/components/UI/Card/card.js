c1c515c9e36625b00ed9326817f6121524a58fcf
Murilo Reis <muriloreisriosalves@gmail.com>
2020-08-19 16:58:08 -0300

    Adding Card image and styling project icons position

--- src/components/UI/Card/card.js
+++ src/components/UI/Card/card.js
@@ -5,7 +5,12 @@ import classes from './card.module.css';
 const card = (props) => {
     return (
         <div className={classes.Card} onClick={props.clicked}>
-            {props.children}
+            <div className={classes.Image}>
+                <img src={`data:image/jpeg;base64,${props.image}`} alt=""/>
+            </div>
+            <div>
+                {props.children}
+            </div>
         </div>
     )
 }
