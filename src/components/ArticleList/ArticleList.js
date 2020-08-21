343de986eb00d69170175cd46f1844ae20d5dd1f
Murilo Reis <muriloreisriosalves@gmail.com>
2020-08-21 16:57:41 -0300

    Fixing layout of delete button

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,28 +1,40 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-import Card from '../UI/Card/card';
-import classes from './ArticleList.module.css'
+import Card from "../UI/Card/card";
+import classes from "./ArticleList.module.css";
 
 const ArticleList = (props) => {
-    return (
-        <Fragment>
-            { props.articles.map(article => {
-                return (
-                    <Card 
-                        key={article._id} 
-                        clicked={() => props.openArticle(article._id)}
-                        image={article.image ? article.image.toString() : null}>
-                        {props.admin ? 
-                            <div className={classes.Delete}>
-                                <button onClick={(e) =>{ e.stopPropagation(); props.onDeleteArticle(article._id)}}><i className="fas fa-trash"></i></button>
-                            </div>: null}   
-                        <h1>{article.title}</h1>
-                        <p>{article.content}</p>
-                    </Card>
-                )
-            })}
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      {props.articles.map((article) => {
+        return (
+          <Card
+            key={article._id}
+            clicked={() => props.openArticle(article._id)}
+            image={article.image ? article.image.toString() : null}
+          >
+            <div className="ProjectTitle">
+              <h1>{article.title}</h1>
+              {props.admin ? (
+                <div className={classes.Delete}>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      props.onDeleteArticle(article._id);
+                    }}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </button>
+                </div>
+              ) : null}
+            </div>
+
+            <p>{article.content}</p>
+          </Card>
+        );
+      })}
+    </Fragment>
+  );
+};
 
 export default ArticleList;
