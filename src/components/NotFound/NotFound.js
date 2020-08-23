4368262df321efd1b4e1577875fa22097346a877
Murilo Reis <muriloreisriosalves@gmail.com>
2020-08-23 12:10:41 -0300

    Styling not found component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const NotFound = () =>{
-    return (
-        <div>
-            <p>404 page not found!</p>
-        </div>
-    )
-}
+import classes from "./NotFound.module.css";
+import Logo from "../Layout/Logo/Logo";
+
+const NotFound = () => {
+  return (
+    <div className={classes.NotFound}>
+      <div className={classes.Logo}>
+        <NavLink to="/" exact>
+          <Logo width="100px" height="100px" />
+        </NavLink>
+      </div>
+      <h1>Page not found! <NavLink to="/" exact>Try Again</NavLink></h1>
+    </div>
+  );
+};
 
 export default NotFound;
